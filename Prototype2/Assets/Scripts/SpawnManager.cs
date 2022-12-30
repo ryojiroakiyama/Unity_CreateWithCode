e0da2ccaf2ead1734a9c8b26221689ce52e75983
ryojiroakiyama <ryojiro25@gmail.com>
2022-12-30 14:42:15 +0900

    spawn random animal

--- Prototype2/Assets/Scripts/SpawnManager.cs
+++ Prototype2/Assets/Scripts/SpawnManager.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-    public int animalIndex;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +14,8 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+
         if (Input.GetKeyDown(KeyCode.S)) {
             Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
         }
